4fc00a252b7d2472a9f4b5340687638d2581b7fe
ofemiashiru <ofemiashiru@gmail.com>
2023-03-05 20:07:11 +0000

    using callbacks

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 
-var xhr = new XMLHttpRequest //XMLHttpRequest allows us to consume APIs by opening, sending and closing connections
+function getData(cb){
 
-xhr.open('GET', 'https://ci-swapi.herokuapp.com/api/')
-xhr.send()
+    var xhr = new XMLHttpRequest //XMLHttpRequest allows us to consume APIs by opening, sending and closing connections
 
+    xhr.open('GET', 'https://ci-swapi.herokuapp.com/api/')
+    xhr.send()
 
-xhr.onreadystatechange = function(){ // when the state changes
-
-    if(this.readyState == 4 && this.status == 200) { // we want to check whether our state changes 
-        // 4 means the operation is complete - https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
-        // 200 means request succeeded content delivered
-
-        // if the state changes after performing a check then update the div with id data with the response we get 
-        // back (respsonseText)
-        document.querySelector('#data').innerHTML = xhr.responseText
+    xhr.onreadystatechange = function(){ // when the state changes
+        console.log(this.readyState) //shows whats happening in readyState
     
-    } 
+        if(this.readyState == 4 && this.status == 200) { // we want to check whether our state changes 
+            // 4 means the operation is complete - https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+            // 200 means request succeeded content delivered
+    
+            // if the state changes after performing a check then update the div with id data with the response we get 
+            // back (respsonseText)
+
+            cb(JSON.parse(this.responseText))
+        
+        } 
+    }
 }
 
+function printDataToConsole(data){
+    console.log(data)
+}
+
+getData(printDataToConsole)
