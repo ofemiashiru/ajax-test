4b3572711be191a24a92cd04836b17a2a7a0f551
ofemiashiru <ofemiashiru@gmail.com>
2023-03-05 21:30:17 +0000

    finish promises

--- promise.js
+++ promise.js
@@ -11,6 +11,18 @@ async function getData(){
 }
 
 
+async function getSpecific(id){
+    let data;
+
+    const resposne = await fetch(`https://ci-swapi.herokuapp.com/api/people/${id}`)
+
+    data = await resposne.json()
+
+    return data //return a promise so when we run the getData() function it must be follow with a .then
+
+}
+
+
 let btn = document.querySelector('#btn')
 
 btn.addEventListener('click', function(){
@@ -18,7 +30,17 @@ btn.addEventListener('click', function(){
     getData()
     .then(data => { // this has to be used as the function is return a promise
         document.querySelector('#data').innerHTML += `<li>${data.people}</li>`
-        
+    })
+
+    getSpecific(20)
+    .then( data => {
+        document.querySelector('#specific').innerHTML = `
+            <p>${data.name}</p>
+            <p>${data.gender}</p>
+            <p>${data.species}</p>
+            <p>${data.skin_color}</p>
+        `
+        console.log(data)
     })
     
 })
