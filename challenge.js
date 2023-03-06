87bbcc987d6f7cb1a9b13d8138308b5c5d722b21
ofemiashiru <ofemiashiru@gmail.com>
2023-03-06 00:14:13 +0000

    api challenge

--- challenge.js
+++ challenge.js
@@ -10,8 +10,29 @@ async function getData(type){
     return data
 }
 
-getData('people')
-.then(data => {
-    document.querySelector('#data').innerHTML = data
-    console.log(data)
+function writeToDocument(type){
+    document.querySelector('#data').innerHTML = '' // empty div before new list
+
+    getData(type)
+    .then(data => {
+        let allData = data.results
+        
+        allData.map((each)=>{
+            
+            return  document.querySelector('#data').innerHTML += `<p>${each.name}</p>`
+        })
+        
+    })
+
+}
+
+let buttons = document.querySelectorAll('button')
+
+buttons.forEach((button)=>{
+    button.addEventListener('click', function(e){
+
+        let type = this.getAttribute('data-type')
+
+        writeToDocument(type)
+    })
 })
