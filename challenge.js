2e394a85f492c51a92e72eb463aa012d9fdcb4e0
ofemiashiru <ofemiashiru@gmail.com>
2023-03-06 00:45:56 +0000

    table headers from object

--- challenge.js
+++ challenge.js
@@ -10,26 +10,40 @@ async function getData(type){
     return data
 }
 
+function getTableHeaders(obj){
+
+    let tableHeaders = []
+
+    Object.keys(obj).forEach((key) => {
+        tableHeaders.push(`<th>${key}</th>`)
+    })
+
+    return `<tr>${tableHeaders}</tr>`
+}
+
 function writeToDocument(type){
     document.querySelector('#data').innerHTML = '' // empty div before new list
 
     getData(type)
     .then(data => {
         let allData = data.results
-        
+        let tableHeaders = getTableHeaders(allData[0])
+
         allData.map((each)=>{
-            
-            return  document.querySelector('#data').innerHTML += `<p>${each.name}</p>`
+            // return  document.querySelector('#data').innerHTML += `<p>${each.name}</p>`
         })
-        
+
+        document.querySelector('#data').innerHTML = `<table>${tableHeaders}</table>`
     })
 
 }
 
+
+
 let buttons = document.querySelectorAll('button')
 
 buttons.forEach((button)=>{
-    button.addEventListener('click', function(e){
+    button.addEventListener('click', function(){
 
         let type = this.getAttribute('data-type')
 
